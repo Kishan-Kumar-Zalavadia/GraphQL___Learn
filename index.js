fdfa47d6d9260e9bb6bb0f4906c79f42fa67ab1f
Kishan-Kumar-Zalavadia <kishankumarz1048@gmail.com>
2022-12-29 13:44:01 +0530

    Adding an enumaration type

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
-    type SkiDay{
-        id: ID!
-        date: String!
-        mountain: String!
-    },
+  type SkiDay {
+    id: ID!
+    date: String!
+    mountain: String!
+    conditions: Conditions
+  }
+
+  enum Conditions{
+    POWDER
+    HEAVY
+    ICE
+    THIN
+  }
+
   type Query {
     totalDays: Int!
     allDays: [SkiDay!]!
